df76812f2c41ca68f125bbab8238e5fa4c7e74a5
manish-patodiya <mpatodiya2@gmail.com>
2021-11-23 12:07:16 +0530

    add signup view

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,10 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        return view('auth/signup');
+    }
+    public function signUp()
+    {
+        return view('auth/signup');
     }
 }
